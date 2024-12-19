2c82405d0f831866bbd3ddbe173cc7df1d3c137a
cl78 <cl78@njit.edu>
2024-12-18 21:58:07 -0500

    fixed upload.py, lots of changes

--- upload.py
+++ upload.py
@@ -6,29 +6,29 @@ def main():
    # Create a client with the MinIO server playground, its access key
    # and secret key.
    client = Minio(
-       "minio:9000",
+       "localhost:9000",
        access_key="u66x9tZKzLWXwd96YHbZ",
        secret_key="2D6K04vua6D07w2y2ejPymwLy4CVrwow2mVtay9d",
        secure=False,
    )
 
    # Make 'demo' bucket if not exist.
-   found = client.bucket_exists("demo")
+   found = client.bucket_exists("profilephotos")
    if not found:
-       client.make_bucket("demo")
+       client.make_bucket("profilephotos")
    else:
-       print("Bucket 'demo' already exists")
+       print("Bucket 'profilephotos' already exists")
 
    # Upload 'minio.jpg' as object name
    # 'minio.jpg' to bucket 'demo'.
    client.fput_object(
        "profilephotos",
-       "minio.jpg",
-       "./minio.jpg",
+       "IMG_0490.HEIC",
+       "/Users/christianlee/Downloads/IMG_0490.HEIC"
    )
    print(
-       "'minio.jpg' is successfully uploaded as "
-       "object 'minio.jpg' to bucket 'demo'."
+       "'IMG_0490.HEIC' is successfully uploaded as "
+       "object 'img_0490.heic' to bucket 'profilephotos'."
    )
 
 
